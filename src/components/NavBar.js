cca5d62226b3a4af481382687a448e6dcddff186
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2021-07-18 19:29:26 -0300

    Add mobile menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,43 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
-import logo from '../assets/ch-w-logo.png';
+import logo from '../assets/ch-br-logo.png';
 
 function NavBar() {
+    const [ click, setClick ] = useState(false);
     return (
-        <nav className='nav'>
+        <nav className='navbar'>
             <div className='container'>
-            <Link to='/' >
-                 <img src={logo} alt='Coffee House logo' className='logo' />
-            </Link>
-            <ul className='nav-list'>
-                <li className='nav-item'>
-                    <Link to='/' className='nav-link'>
-                        HOME
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/menu' className='nav-link'>
-                        MENU
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/shop' className='nav-link'>
-                        SHOP
-                    </Link>
-                </li>
-                <li className='nav-item'>
-                    <Link to='/' className='nav-link'>
-                        MORE <i class="fas fa-caret-down"></i>
-                    </Link>
-                </li>
-            </ul>
+                <Link to='/' >
+                    <img src={logo} alt='Coffee House logo' className='logo' />
+                </Link>
+                <div className={click ? 'menu-icon active' : 'menu-icon'} onClick={() => setClick(!click)}>
+                    <div class="line top"></div>
+                    <div class="line middle"></div>
+                    <div class="line bottom"></div>
+                </div>
+                <ul className={click ? 'nav-list active' : 'nav-list'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-link'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/menu' className='nav-link'>
+                            Menu
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/shop' className='nav-link'>
+                            Shop
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-link'>
+                            More <i class="fas fa-caret-down"></i>
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     )

56574707c0539d2fa712e3ed20777928c0a1cfb7
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2021-07-27 23:36:34 -0300

    Update Image Slider component

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -7,31 +7,49 @@ const ImageSlider = ({ slides }) => {
     const length = slides.length;
 
     const previousSlide = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1); 
-    }
-    const nextSlide = () => {
         setCurrent(current === 0 ? length - 1 : current - 1); 
     }
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1); 
+    }
 
-    if(!Array.isArray(slides) || slides.length <= 0) {
+    if(!Array.isArray(slides) || length <= 0) {
         return null;
     }
     return (
-        <section className='slider'>
-            <div className='leftArrowBtn' onClick={previousSlide}>
-                <i className='fas fa-chevron-left'></i>
-            </div>
+        <div className='slider'>
                 {SliderData.map((slide, index) => {
                     return (
-                        <div className={index === current ? 'current slide' : 'slide'} key={index}>
-                            {index === current && <img src={slide.image} className='image' alt='Coffee'/>}
-                        </div>
+                        <>
+                            <div className={index === current ? 'current slide' : 'slide'} key={index}>
+                                {index === current && <img src={slide.image} className='image' alt='Coffee'/>}
+                            </div>
+                            <div className='dots-container'>
+                                <button
+                                    className={current === 0 ? 'active slider-dot' : 'slider-dot'}
+                                    onClick={() => setCurrent(0)}>
+                                </button>
+                                <button
+                                    className={current === 1 ? 'active slider-dot' : 'slider-dot'}
+                                    onClick={() => setCurrent(1)}>
+                                </button>
+                                <button
+                                    className={current === 2 ? 'active slider-dot' : 'slider-dot'}
+                                    onClick={() => setCurrent(2)}>
+                                </button>
+                            </div>
+                        </>
                     )
                 })}
-            <div className='rightArrowBtn' onClick={nextSlide}>
-                <i className='fas fa-chevron-right'></i>
+            <div className='button-container'>
+                <button className='arrowBtn' onClick={previousSlide}>
+                    <i className='fas fa-chevron-left'></i>
+                </button>    
+                <button className='arrowBtn right' onClick={nextSlide}>
+                    <i className='fas fa-chevron-right'></i>
+                </button>
             </div>
-        </section>
+        </div>
     )
 };
 
--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,13 +1,15 @@
-import cup from '../assets/photos/cup.png';
+import cup1 from '../assets/photos/cup1.png';
+import cup2 from '../assets/photos/cup2.png';
+import cup3 from '../assets/photos/cup3.png';
 
 export const SliderData = [
     {
-        image: cup
+        image: cup1
     },
     {
-        image: cup
+        image: cup2
     },
     {
-        image: cup
+        image: cup3
     },
 ]

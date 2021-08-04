87f238ccd26c973a7ab5f97892fbe19b9453e1b3
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2021-08-04 19:35:44 -0300

    Add Home page mobile app conteiner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ import React from 'react';
 import './Home.css';
 import ImageSlider from '../components/ImageSlider.js';
 import { SliderData } from '../components/SliderData';
-import plant from '../assets/photos/plant.jpg';
+import chApp from '../assets/ch-app.png';
+import smartphone from '../assets/photos/smartphone.png';
 
 // We always work with an environmental conscience, using recycled products, and practicing compost, improving soil health, reducing greenhouse gas emissions, recycling nutrients, and mitigating the impact of droughts. We reap what we sow and we want the best future for our planet.
 
@@ -34,13 +35,23 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className='panel'>
+            <div className='panel-container'>
                 <div className='panel-content'>
                     <h1 className='panel-title'>Eco-Friendly</h1>
                     <p  className='panel-text'>We reap what we sow and we want the best future for our planet.</p>
                     <button  className='panel-button'>Learn more</button>
                 </div>
                 <div className='image-container'></div>
+            </div>
+            <div className='app-container'>
+                <div className='app-image'>
+                    <img src={smartphone} className='smartphone'/>
+                </div>
+                <div className='app-content'>
+                    <h1 className='app-title'>Order on the app</h1>
+                    <p className='app-text'>Download our mobile app to enroll and start ordering ahead. Find stores, redeem offers and earn rewards.</p>
+                    <img src={chApp} className='ch-app' alt='CH App Button'/>
+                </div>
             </div>
         </div>
     )
